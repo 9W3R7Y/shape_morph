1c9d9feec57871c6c05998f13292a6ea9c7afe10
qwerty_16180339 <48379338+qwerty16180339@users.noreply.github.com>
2021-03-12 22:49:59 +0900

    Create obj2img.m

---
+++ obj2img.m
@@ -0,0 +1,30 @@
+function [layer_img,layer_mask] = obj2img(obj,shape)
+    blender = vision.AlphaBlender('Operation','Binary mask','MaskSource','Input port');
+    
+    img = im2single(obj{2});
+    mask = im2single(obj{3});
+    vert = obj{5};
+    meshes = obj{6};
+
+    [W,H,~] = size(img);
+    layer_img = single(ones(size(img))).*0.5;
+    layer_mask = single(zeros([W,H]));
+
+    [mesh_num,~] = size(meshes);
+
+    for i = 1:mesh_num
+        idxs = meshes(i,:);
+        mesh = vert(idxs,:);
+        mesh_mask = mask.*poly2mask(mesh(:,1),mesh(:,2), W, H);
+
+        T = transCoeff(mesh,mesh);
+        tform = affine2d(T);
+        outView = affineOutputView([W,H],tform,'BoundsStyle','sameAsInput');
+
+        mesh_img = imwarp(img,tform,'OutputView',outView);
+        mesh_mask = imwarp(mesh_mask,tform,'OutputView',outView);
+
+        layer_img = blender(layer_img,mesh_img,mesh_mask);
+        layer_mask = layer_mask+mesh_mask;
+    end
+end
